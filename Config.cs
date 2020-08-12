0f86a7930a52d8bc08aa380fcb912d5ac1f48ae1
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-11 19:24:22 -0500

    v1.0.0 Release

---
+++ Config.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace SpawnBroadcasts
+{
+    public class Config : IConfig
+    {
+        [Description("Leave a class setting blank to disable spawn broadcasts for that class.")]
+        public bool IsEnabled { get; set; } = false;
+        public int BroadcastDuration { get; set; } = 10;
+        public string ClassD { get; set; } = "";
+        public string Scientist { get; set; } = "";
+        public string FacilityGuard { get; set; } = "";
+        public string MtfCadet { get; set; } = "";
+        public string MtfLieutenant { get; set; } = "";
+        public string MtfScientist { get; set; } = "";
+        public string MtfCommander { get; set; } = "";
+        public string Tutorial { get; set; } = "";
+        public string Scp049 { get; set; } = "";
+        public string Scp0492 { get; set; } = "";
+        public string Scp079 { get; set; } = "";
+        public string Scp096 { get; set; } = "";
+        public string Scp106 { get; set; } = "";
+        public string Scp173 { get; set; } = "";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace SpawnBroadcasts
+{
+    public class Plugin : Plugin<Config>
+    {
+        private static readonly Lazy<Plugin> LInstance = new Lazy<Plugin>(() => new Plugin());
+        public static Plugin Instance => LInstance.Value;
+        private Plugin()
+        {
+        }
+
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        public override string Author { get; } = "DGvagabond";
+        public override string Name { get; } = "SpawnBroadcasts";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
+
+        private Handlers.Player PlayerEvents;
+        public static Plugin Singleton;
+
+        public override void OnEnabled()
+        {
+            try
+            {
+                RegisterEvents();
+                Log.Info($"v{Version}, made by {Author}, successfully loaded.");
+            }
+
+            catch (Exception e)
+            {
+                Log.Error($"There was an error loading the plugin: {e}");
+            }
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+        }
+
+        public void RegisterEvents()
+        {
+            Singleton = this;
+            PlayerEvents = new Handlers.Player(this);
+
+            Player.ChangingRole += PlayerEvents.OnSpawn;
+        }
+        public void UnregisterEvents()
+        {
+            Player.ChangingRole -= PlayerEvents.OnSpawn;
+
+            PlayerEvents = null;
+        }
+    }
+}
